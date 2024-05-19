02f05ee88436b6d1391e9b44a3896454229cc34d
FreeFly0125 <simonbergman516@gmail.com>
2024-05-19 18:30:29 +0200

    feat: interact with postgres db

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
+from app.api.routes.routes import api_router
 
 app = FastAPI()
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -8,3 +12,16 @@ def good_luck():
     return {
         "Good": "Luck!",
     }
+
+
+@app.get(path="/health", status_code=200)
+def checkServerHealth():
+    return {"status": "Hello, World"}
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
