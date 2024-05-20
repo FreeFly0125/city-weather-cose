84d8aba87016e608ee7c5b67392d9541346e08fd
FreeFly0125 <simonbergman516@gmail.com>
2024-05-20 09:19:21 +0200

    fix: add CORS for server

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api.routes.routes import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
